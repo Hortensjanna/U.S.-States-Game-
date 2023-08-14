adc5f0b7e7df119539700f135ded9c732646b7f0
Ania Płecka <aniaplecka@gmail.com>
2023-08-14 11:28:42 +0200

    Score tracking and taking another answer

--- main.py
+++ main.py
@@ -10,21 +10,21 @@ turtle.shape(image)
 data = pandas.read_csv("50_states.csv")
 
 correct_states = 0
-answer_state = screen.textinput(title=f"{correct_states}/50 States Correct", prompt="What's another state's name?")
+game_is_on = True
+while game_is_on:
+    answer_state = screen.textinput(title=f"{correct_states}/50 States Correct", prompt="What's another state's name?")
 
+    current_state = data[data.state == answer_state]
+    state_name = current_state.state
+    x_cor = int(current_state.x)
+    y_cor = int(current_state.y)
+    correct_states += 1
 
-current_state = data[data.state == answer_state]
-state_name = current_state.state
-x_cor = int(current_state.x)
-y_cor = int(current_state.y)
-
-
-writing = turtle.Turtle()
-writing.penup()
-writing.hideturtle()
-writing.goto(x_cor, y_cor)
-writing.write(f"{answer_state}", align="center", font=("Arial", 8, "normal"))
-
+    writing = turtle.Turtle()
+    writing.penup()
+    writing.hideturtle()
+    writing.goto(x_cor, y_cor)
+    writing.write(f"{answer_state}", align="center", font=("Arial", 8, "normal"))
 
 
 
